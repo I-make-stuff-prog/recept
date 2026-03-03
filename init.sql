7bd16e03375d9d06df7402a424ab11162c965d61
S.Olivér <72759068186@tanulo.bankisuli.hu>
2026-03-03 07:51:19 +0100

    Delete init.sql

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
-    email VARCHAR(100)
-);
-
-INSERT INTO users (name, email)
-VALUES ('Alice', 'alice@email.com');
